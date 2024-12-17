5cba2af41da04c588ba16143383285127ec23994
Spectrality <54081042+spectrality-github@users.noreply.github.com>
2024-12-17 21:57:23 +0200

    43

--- wmbusmeters-ha-addon-edge/run.sh
+++ wmbusmeters-ha-addon-edge/run.sh
@@ -16,12 +16,12 @@ then
     bashio::addon.restart
 fi
 
-if bashio::config.exists "MbusTTY"
-then
-    bashio::log.info "Removing MbusTTY Option"
-    bashio::addon.option "MbusTTY"
-fi
-bashio::addon.option "MbusTTY"
+#if bashio::config.exists "MbusTTY"
+#then
+#    bashio::log.info "Removing MbusTTY Option"
+#    bashio::addon.option "MbusTTY"
+#fi
+
 
 CONFIG_DATA_PATH=$(bashio::jq "${CONFIG_PATH}" '.data_path')
 CONFIG_CONF=$(bashio::jq "${CONFIG_PATH}" '.conf')
@@ -115,15 +115,15 @@ python3 /flask/app.py &
 if [ "$(bashio::config 'MbusTCPenabled')" = "yes" ]
 then
     bashio::log.info "Running socat ..."
-    bashio::log.info "while true; do socat pty,group-late=tty,link="$(bashio::config 'MbusTCPtty')",mode=660,rawer,echo=0,b"$(bashio::config 'MbusTCPttyBaud')",waitslave,ignoreeof tcp:"$(bashio::config 'MbusTCPhost')":"$(bashio::config 'MbusTCPhostPort')"; done&"
+    bashio::log.info "while true; do socat pty,group-late=tty,link="$(bashio::config 'MbusTCPtty')",mode=660,rawer,echo=0,b"$(bashio::config 'MbusTCPttyBaud')",waitslave,ignoreeof tcp:"$(bashio::config 'MbusTCPhost')":"$(bashio::config 'MbusTCPhostPort')"; done &"
     #while true; do socat pty,group-late=tty,link=/root/ttyMBUS0,mode=660,rawer,echo=0,b2400,waitslave,ignoreeof tcp:192.168.3.119:2003; done&
-    while true; do socat pty,group-late=tty,link="$(bashio::config 'MbusTCPtty')",mode=660,rawer,echo=0,b"$(bashio::config 'MbusTCPttyBaud')",waitslave,ignoreeof tcp:"$(bashio::config 'MbusTCPhost')":"$(bashio::config 'MbusTCPhostPort')"; done&
+    while true; do socat pty,group-late=tty,link="$(bashio::config 'MbusTCPtty')",mode=660,rawer,echo=0,b"$(bashio::config 'MbusTCPttyBaud')",waitslave,ignoreeof tcp:"$(bashio::config 'MbusTCPhost')":"$(bashio::config 'MbusTCPhostPort')"; done &
 fi
 
 if [ "$(bashio::config 'MbusTCPenabled')" = "yesStatic" ]
 then
     bashio::log.info "Running socat ..."
-    while true; do socat pty,group-late=tty,link=/root/ttyMBUS0,mode=660,rawer,echo=0,b2400,waitslave,ignoreeof tcp:192.168.3.119:2003; done&
+    while true; do socat pty,group-late=tty,link=/root/ttyMBUS0,mode=660,rawer,echo=0,b2400,waitslave,ignoreeof tcp:192.168.3.119:2003; done &
 fi
 
 bashio::log.info "Running wmbusmeters ..."
