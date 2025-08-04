94cea5361e0db324acb3e74b222e4d1525853b79
Spectrality <54081042+spectrality-github@users.noreply.github.com>
2025-08-04 12:56:09 +0300

    Update run.sh

--- wmbusmeters-ha-addon-edge/run.sh
+++ wmbusmeters-ha-addon-edge/run.sh
@@ -124,7 +124,7 @@ then
     #while true; do socat pty,group-late=tty,link=/root/ttyMBUS0,mode=660,rawer,echo=0,b2400,waitslave,ignoreeof tcp:192.168.3.119:2003; done&
     
     remaining_attemps=5
-    while ( remaining_attemps-- > 0 )
+    while (( remaining_attemps-- > 0 ))
     do
         if [ ! -f $MbusTCPtty ]
         then
