59cfa3209ad960e2821cb9746210a85966056864
Spectrality <54081042+spectrality-github@users.noreply.github.com>
2025-09-26 20:18:34 +0300

    Update run.sh

--- wmbusmeters-ha-addon-edge/run.sh
+++ wmbusmeters-ha-addon-edge/run.sh
@@ -120,10 +120,16 @@ then
     fi    
     bashio::log.info "Running socat ..."
     bashio::log.info "while true; do socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort; done"
-    while true; do socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort; done&
+    while true
+    do 
+        bashio::log.info "Running socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort"
+        #bashio::log.info "while true; do socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort; done"
+        socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort 
+    done&
     #while true; do socat pty,group-late=tty,link=/root/ttyMBUS0,mode=660,rawer,echo=0,b2400,waitslave,ignoreeof tcp:192.168.3.119:2003; done&
 
     bashio::log.info "Checking is MbusTCPtty device already available"
+    sleep 1
     remaining_attempts=5
     while (( remaining_attempts-- > 0 ))
     do
