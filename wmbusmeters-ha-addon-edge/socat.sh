b2fe2b69749db7ce92f7de1a3ed64b0d3142c90b
Spectrality <54081042+spectrality-github@users.noreply.github.com>
2025-09-28 18:25:10 +0300

    Update socat.sh

--- wmbusmeters-ha-addon-edge/socat.sh
+++ wmbusmeters-ha-addon-edge/socat.sh
@@ -7,16 +7,16 @@ then
     MbusTCPhost=$(bashio::config 'MbusTCPhost')
     MbusTCPhostPort=$(bashio::config 'MbusTCPhostPort')
 
-    bashio::log.info "Testing is MbusTCP accessible ..."
+    bashio::log.info "(socat) Testing is MbusTCP accessible ..."
     if nc -w 3 -z $MbusTCPhost $MbusTCPhostPort > /dev/null
     then
-        bashio::log.info "$MbusTCPhost:$MbusTCPhostPort MbusTCP is accessible $(dig +short $MbusTCPhost) IP"
-        bashio::log.info "Running socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort"
+        bashio::log.info "(socat) $MbusTCPhost:$MbusTCPhostPort MbusTCP is accessible $(dig +short $MbusTCPhost) IP"
+        bashio::log.info "(socat) Running socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort"
         socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort
     else
-        bashio::log.info "$MbusTCPhost:$MbusTCPhostPort MbusTCP is not accessible $(dig +short $MbusTCPhost) IP"
-        #sleep 5
+        bashio::log.info "(socat) $MbusTCPhost:$MbusTCPhostPort MbusTCP is not accessible $(dig +short $MbusTCPhost) IP"
+        sleep 1
     fi        
    
-    #socat pty,group-late=tty,link=/root/ttyMBUS0,mode=660,rawer,echo=0,b2400,waitslave,ignoreeof tcp:192.168.3.9:2003; done&
+    #socat pty,group-late=tty,link=/root/ttyMBUS0,mode=660,rawer,echo=0,b2400,waitslave,ignoreeof tcp:192.168.3.9:2003
 fi
