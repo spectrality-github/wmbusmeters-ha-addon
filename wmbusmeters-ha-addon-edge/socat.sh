217884a20f1ccd9f710efef9ad2a18f18c6154ac
Spectrality <54081042+spectrality-github@users.noreply.github.com>
2025-09-28 18:30:39 +0300

    Update socat.sh

--- wmbusmeters-ha-addon-edge/socat.sh
+++ wmbusmeters-ha-addon-edge/socat.sh
@@ -10,11 +10,14 @@ then
     bashio::log.info "(socat) Testing is MbusTCP accessible ..."
     if nc -w 3 -z $MbusTCPhost $MbusTCPhostPort > /dev/null
     then
-        bashio::log.info "(socat) $MbusTCPhost:$MbusTCPhostPort MbusTCP is accessible $(dig +short $MbusTCPhost) IP"
+        bashio::log.info "(socat) $MbusTCPhost:$MbusTCPhostPort MbusTCP is accessible on $(dig +short $MbusTCPhost) IP"
         bashio::log.info "(socat) Running socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort"
+        if pgrep socat > /dev/null; then
+            pkill socat
+        fi
         socat pty,group-late=tty,link=$MbusTCPtty,mode=660,rawer,echo=0,b$MbusTCPttyBaud,waitslave,ignoreeof tcp:$MbusTCPhost:$MbusTCPhostPort
     else
-        bashio::log.info "(socat) $MbusTCPhost:$MbusTCPhostPort MbusTCP is not accessible $(dig +short $MbusTCPhost) IP"
+        bashio::log.info "(socat) $MbusTCPhost:$MbusTCPhostPort MbusTCP is not accessible on $(dig +short $MbusTCPhost) IP"
         sleep 1
     fi        
    
